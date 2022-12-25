9ec5eb9664b584f6136ec578cb02aa6ac974a012
Tanisha <tanishagupta0820@gmail.com>
2022-12-25 12:17:27 +0530

    about

---
+++ src/About/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './about.css'
+
+export default function WedAbout() {
+    return (
+        <>
+            <div className="about-container" id="about">
+                <h1 className="about-title">ABOUT</h1>
+                <p className="about-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus tenetur quis nemo! Amet quidem repellat iste consequatur obcaecati dolore eos dolores? Nam animi architecto, rerum unde aspernatur officia quidem eius perspiciatis, ut omnis dolorum delectus ad accusantium libero molestiae? Eveniet officiis eligendi iure maxime porro, magnam harum eius commodi optio veritatis laudantium dolores perferendis aspernatur id, consequatur perspiciatis at accusamus voluptate velit quibusdam in pariatur minus? Quasi dolorem porro culpa accusamus alias consequatur dolore omnis reprehenderit. Distinctio consectetur, eos culpa quam repellendus officiis laboriosam eum perspiciatis, temporibus excepturi rem eligendi in? Voluptatibus vitae accusantium saepe quasi, illo nulla repudiandae ut.</p>
+            </div>
+        </>
+    )
+}
--- src/home.js
+++ src/home.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Topnav from './TopNav/topnav.js';
 import Slideshow from './Slideshow/slideshow.js';
+import WedAbout from './About/about.js';
 
 function Home() {
     return (
         <div style={{ width: "calc(100% - 40px)", backgroundColor: "transparent", height: "100vh" }}>
             <Topnav />
             <Slideshow />
+            <WedAbout />
         </div>
     )
 }
